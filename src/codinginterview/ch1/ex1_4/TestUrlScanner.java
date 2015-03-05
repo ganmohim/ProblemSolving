f53ad8249da60ef2d22a699123eb7e1a6a9b5dfa
G A N MOHIM <gan.mohim@gmail.com>
2015-03-04 22:21:55 -0500

    Submitting url processor
    Input:  "Mr John Smith"
    Output: "Mr%20John%20Smith"

---
+++ src/codinginterview/ch1/ex1_4/TestUrlScanner.java
@@ -0,0 +1,10 @@
+package codinginterview.ch1.ex1_4;
+
+public class TestUrlScanner {
+	public static void main(String[] args) {
+		String url = "Mr John Smith";
+		UrlScanner urlObj = new UrlScanner();
+		urlObj.updateUrl(url);
+	}
+
+}
---
+++ src/codinginterview/ch1/ex1_4/UrlScanner.java
@@ -0,0 +1,35 @@
+package codinginterview.ch1.ex1_4;
+
+import java.util.*;
+
+public class UrlScanner {
+
+    public void updateUrl(String url) {
+        char[] specialChars = {'%', '2', '0'};
+        ArrayList<Character> charListUrl = new ArrayList<Character>();
+        char[] charArrayUrl = url.toCharArray();
+        
+        for (int i = 0; i < charArrayUrl.length; i++) {
+            if (charArrayUrl[i] == ' ') {
+                for (int j = 0; j < specialChars.length; j++) {
+                    charListUrl.add(specialChars[j]); 
+                }
+            
+            } // end of if
+            
+            else {
+                charListUrl.add(charArrayUrl[i]);
+            } // end of else
+            
+        } // end of for
+        
+        
+        for (Character c : charListUrl) {
+            System.out.print(c);
+        }
+        
+        
+    
+    }
+
+}
