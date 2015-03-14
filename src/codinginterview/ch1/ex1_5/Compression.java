c55ed2a59cb75839b5946e3fdaa074e73ffcc845
ganmohim <gan.mohim@gmail.com>
2015-03-14 16:37:18 -0400

    A simple compression alogrithm

---
+++ src/codinginterview/ch1/ex1_5/Compression.java
@@ -0,0 +1,52 @@
+package codinginterview.ch1.ex1_5;
+
+import java.util.*;
+
+public class Compression {
+
+    public String compressString(String strToCompress) {
+        ArrayList<Character> listOfChar = new ArrayList<Character>();
+        int counter = 0;
+        char[] charArray = new char[strToCompress.length()];
+        charArray = strToCompress.toCharArray();
+        
+        for (int i = 0; i < charArray.length; i++) {
+        
+            listOfChar.add(charArray[i]);
+            counter = 1;
+            
+            // bug fix for value like this (abc) --> (i != charArray.length - 1)
+            while ((i != charArray.length - 1) && (charArray[i] == charArray[i+1]) ) {
+                counter++; // 2
+                i++; //6, 7
+                
+                if (i == charArray.length - 1) // bug fix = array out of bound
+                    break;
+            }
+            
+            // Following lines converts from integer to Character (counter value)
+            String n = Integer.toString(counter);
+            listOfChar.add(n.charAt(0));
+            
+        
+        } //end of for
+        
+        
+        String strCompressesd = "";
+        for (Character c : listOfChar) {
+        	strCompressesd = strCompressesd + c;
+        }
+        
+        // This logic kicks when after compression the string equal or larger
+        // than the string that was provided to compress
+        // abc = a1b1c1 --> After compression string is larger than original
+        // abbccc = a1b2c3 --> After compression the size is same before compression
+        if (strCompressesd.length() >=  strToCompress.length())
+            return strToCompress;
+        
+        return strCompressesd;
+    
+    }    
+    
+    
+} // end of Compression
---
+++ src/codinginterview/ch1/ex1_5/TestCompression.java
@@ -0,0 +1,11 @@
+package codinginterview.ch1.ex1_5;
+
+public class TestCompression {
+	public static void main(String[] args) {
+		Compression c = new Compression();
+		String str = c.compressString("aabbccc");
+		System.out.println(str);
+		
+	}
+
+}
